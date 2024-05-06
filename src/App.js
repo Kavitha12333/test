b379677b1b9c0bab2e0fb85de790252970b6d914
kavitha <kavithask8861@gmail.com>
2024-05-06 15:13:19 +0530

    hello

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Home from"./Home"
 function App() {
   return (
     <div className="App">
      <h2>hwlljGb</h2>
+     <Home/>
     </div>
   );
 }
